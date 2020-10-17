0a86bbe19c812102f47b3a49fa8abd016b8b28c7
Robert Schmahl <rschm007@gmail.com>
2020-10-16 19:08:34 -0700

    App now saves text field values to local storage and retrieves them from local storage. Values persist after page reset

--- Assets/script.js
+++ Assets/script.js
@@ -7,19 +7,16 @@
 // (default rows include standard business hours, ie 9am - 5pm. There can also be an input field where the user can change this)
 //
 // I WANT an easy to use interface that tracks the present time so that I can see at a glance where I am in my day
-// (each time-block row should be color-coded to indicate past, present and future)
+// (each time-block row should be color-coded to indicate past, present and future) - DONE
 //
 // I WANT to be able to type in the hour rows so that I can plan my day
-// (each time row should be an editable field)
+// (each time row should be an editable field) - DONE
 //
 // I WANT to be able to save my edits in each row
-// (each row should have a save button that is inserted after user clicks the row. When save is clicked, data is saved in localstorage)
-//
-// Once I have planned my day, I WANT to be able to save the page
-// (Have the URL change to a unique ID when the user answers the initial prompt so that the page persists)
+// (each row should have a save button that is inserted after user clicks the row. When save is clicked, data is saved in localstorage) - DONE
 //
 // IF I EXIT THE PAGE, I want to be able to go right back to what I was doing
-// (all user-inputted data should be saved to localstorage)
+// (all user-inputted data should be saved to localstorage) - DONE
 
 // ======================================= begin working JS==============================================
 $(document).ready(function () {
@@ -51,9 +48,9 @@ $(document).ready(function () {
       $(this).addClass("present");
     }
   });
-})
-// begin work on creating localstorage saves and gets for save button clicks
-// define vars containing buttons
+
+  // begin work on creating localstorage saves and gets for save button clicks
+  // define vars containing buttons
   var $row9am = $("#9amForm");
   var $row10am = $("#10amForm");
   var $row11am = $("#11amForm");
@@ -64,17 +61,28 @@ $(document).ready(function () {
   var $row4pm = $("#4pmForm");
   var $row5pm = $("#5pmForm");
 
-// add an event listener for clicking a save button
-$("button").on("click", function(event) {
+  // add an event listener for clicking a save button
+  $("button").on("click", function (event) {
     // set items in local storage
-    localStorage.setItem("9am", ($row9am.val()))
-    localStorage.setItem("10am", ($row10am.val()))
-    localStorage.setItem("11am", ($row11am.val()))
-    localStorage.setItem("12pm", ($row12pm.val()))
-    localStorage.setItem("1pm", ($row1pm.val()))
-    localStorage.setItem("2pm", ($row2pm.val()))
-    localStorage.setItem("3pm", ($row3pm.val()))
-    localStorage.setItem("4pm", ($row4pm.val()))
-    localStorage.setItem("5pm", ($row5pm.val()))
-});
+    localStorage.setItem("9am", $row9am.val());
+    localStorage.setItem("10am", $row10am.val());
+    localStorage.setItem("11am", $row11am.val());
+    localStorage.setItem("12pm", $row12pm.val());
+    localStorage.setItem("1pm", $row1pm.val());
+    localStorage.setItem("2pm", $row2pm.val());
+    localStorage.setItem("3pm", $row3pm.val());
+    localStorage.setItem("4pm", $row4pm.val());
+    localStorage.setItem("5pm", $row5pm.val());
+  });
 
+  // retrieve localstorage values and insert them into text fields. Text field values should persist through refreshes
+  $("#9amForm").text(localStorage.getItem("9am"));
+  $("#10amForm").text(localStorage.getItem("10am"));
+  $("#11amForm").text(localStorage.getItem("11am"));
+  $("#12pmForm").text(localStorage.getItem("12pm"));
+  $("#1pmForm").text(localStorage.getItem("1pm"));
+  $("#2pmForm").text(localStorage.getItem("2pm"));
+  $("#3pmForm").text(localStorage.getItem("3pm"));
+  $("#4pmForm").text(localStorage.getItem("4pm"));
+  $("#5pmForm").text(localStorage.getItem("5pm"));
+});
